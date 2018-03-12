f5178f6bffba9202e49a0dbbf6ce51e7cd3f9202
sivacharansrc@gmail.com <sivacharansrc@gmail.com>
2018-03-12 16:20:41 -0500

    Data Cleaning and Manipulation Changes

--- rCode/BigMartSales_1.R
+++ rCode/BigMartSales_1.R
@@ -44,25 +44,17 @@ summariseCategCols <- function(x,y){
     respVar <- which(colnames(x) == y)
     if(length(unique(x[[which(colnames(x) == colName)]])) < 51) {
       #df1 <-  z[,.(Total_Sum = sum(z[,respVar], na.rm=T), Total_Count = .N), colName]
-      df1 <-  z[,.(Total_Count = .N), colName]
+      df1 <-  z[,.(Total_Count = .N, Count_Percent = round((.N*100/nrow(z)),0)), colName]
       names(df1)[1] <- "Category_Values"
       df1$Category <- colName
-      df1 <- df1[,c(3,1,2)]
+      df1 <- df1[,c(4,1,2,3)]
       df <- rbind(df,df1)
     }
   }
   return(df)
 }
 
-summariseCategCols(train, "Item_Outlet_Sales")
-
-x <- train
-y <- "Item_Outlet_Sales"
-
-train[,.(Total_Sum = sum(c("Item_Outlet_Sales"))), Outlet_Type]
-
-gsub("a","","Sivai")
-
+#summariseCategCols(train, "Item_Outlet_Sales")
 
 
 ####### READING DATA ######
@@ -72,9 +64,10 @@ gsub("a","","Sivai")
 train <- fread("./src/train.csv", sep = ",", header = T, na.strings = c("",NA," ","NA", "N/A"))
 test <- fread("./src/test.csv", sep = ",", header = T, na.strings = c("",NA," ","NA", "N/A"))
 
-####### DATA MANIPULATION AND FEATURE ENGINEERING #########
-# str(cleanData)
-# summary(cleanData)
+
+### DATA CLEANING ####
+
+# CLEANING UP TRAIN DATA TO PICK ONLY THE ITEM IDENTIFIERS AVAILABLE IN TEST
 
 itemIdentifierList <- unique(test$Item_Identifier)
 clean.train <- train[Item_Identifier %in% itemIdentifierList,]
@@ -91,36 +84,63 @@ outletIDDf <- data.frame(ItemId = cleanData$Outlet_Identifier ,
                          ItemIDCode = as.integer(as.factor(cleanData$Outlet_Identifier)))
 outletIDDf <- unique(outletIDDf)
 
-## Converting all character columns to integers: names(cleanData) View(head(cleanData))
+##  names(cleanData) View(head(cleanData))  summary(cleanData)  str(cleanData) View(cleanData)
 
-summariseCategCols(clean.train,"Item_Outlet_Sales")
+## RECODING VALUES IN THE ITEM FAT CONTENT
 
+cleanData[,Item_Fat_Content_Fixed := ifelse(Item_Fat_Content %in% c("Regular", "reg"),"Regular","Low Fat")]
+
+#### IMPUTING MISSING VALUES  ####
+
+# OUTLET SIZE AND ITEM WEIGHT HAS MISSING VALUES
+# OUTLET SIZE = MODE OF OUTLET SIZE BY OUTLET TYPE
+# ITEM WEIGHT = MEAN OF ITEM WEIGHT BY ITEM TYPE
+# ITEM VISIBIILTY DOES NOT HAVE MISSING VALUES BUT HAS 0 WHICH CANNOT BE THE CASE. 
+# CONVERTING ALL ITEM VISIBILITY WITH 0 AS NA, AND IMPUTING THE MISSING VALUES BY CALCULATING MEAN OF ITEM VISIBILITY BY OUTLET AND ITEM TYPE
+
+
+### OUTLET SIZE
 cleanData[,Outlet_Size_Fixed := ifelse(!is.na(Outlet_Size), Outlet_Size, Mode(Outlet_Size)), Outlet_Type]
 
-### FOLLOW UP FROM HERE
 
-
-
-
-
-
-
-
-
-
-
-
+### ITEM WEIGHT
 cleanData[,Item_Weight_Fixed1 := mean(Item_Weight, na.rm = T), Item_Identifier][,Item_Weight_Fixed:= ifelse(is.na(Item_Weight),Item_Weight_Fixed1,Item_Weight)]
-cleanData[,Item_Fat_Content_Fixed := ifelse(Item_Fat_Content %in% c("Regular", "reg"),"Regular","Low Fat")]
+
+### ITEM VISIBILITY
+
+cleanData[Item_Visibility == 0, Item_Visibility := NA]
+cleanData[,Item_Visibility_Fixed := ifelse(!is.na(Item_Visibility), Item_Visibility, mean(Item_Visibility, na.rm = T)), .(Outlet_Identifier, Item_Type)]
+
+
+### FEATURE ENGINEERING ####
+
+cleanData[, Item_Type_New := ifelse(Item_Type %in% c("Soft Drinks", "Hard Drinks"),
+                                    "Drinks",
+                                    ifelse(Item_Type %in% c("Household", "Baking Goods","Health and Hygiene", "Others"),
+                                           "Others", "Food"))]
+
+## CREATING NA REFERENCE COLS WHEREVER APPLICABLE
+cleanData[, Outlet_Size_NA := ifelse(is.na(Outlet_Size),0,1)][, Item_Visibility_NA := ifelse(is.na(Item_Visibility),0,1)][, Item_Weight_NA := ifelse(is.na(Item_Weight),0,1)]
+
+## CREATING NEW COLUMN - NO OF YRS THE OUTLET IS OPERATIONAL
+cleanData[,No_of_Yrs := 2013 - Outlet_Establishment_Year]
+
+## HOT ENCODING
+cleanData <- data.table(dummy.data.frame(cleanData, names = c("Item_Fat_Content_Fixed", "Outlet_Type", "Outlet_Location_Type", "Outlet_Size_Fixed", "Item_Type_New"), sep="_"))
+
 cleanData$Item_Identifier <- as.integer(as.factor(cleanData$Item_Identifier))
-#cleanData[,Sales_By_Item_Type := sum(Item_Outlet_Sales, na.rm = T),Item_Type]
-cleanData <- data.table(dummy.data.frame(cleanData, names = c("Item_Fat_Content_Fixed", "Outlet_Type", "Outlet_Location_Type"), sep="_"))
+
 cleanData$Item_Type <- as.integer(as.factor(cleanData$Item_Type))
+
 cleanData$Outlet_Identifier <- as.integer(as.factor(cleanData$Outlet_Identifier))
-cleanData$Outlet_Size <- as.integer(as.factor(cleanData$Outlet_Size))
-cleanData[, Outlet_Size_NA := ifelse(is.na(Outlet_Size),0,1)][,No_of_Yrs := 2013 - Outlet_Establishment_Year]
-cleanData[is.na(Outlet_Size), Outlet_Size:= -999]
-cleanData[,c("Outlet_Establishment_Year", "Item_Weight_Fixed1", "Item_Weight", "Item_Fat_Content"):= NULL]
+
+#cleanData[,Sales_By_Item_Type := sum(Item_Outlet_Sales, na.rm = T),Item_Type]
+
+# View(cleanData) names(cleanData)
+
+cleanData[, c("Outlet_Establishment_Year", "Item_Weight_Fixed1", "Item_Weight", "Item_Fat_Content",
+              "Item_Visibility", "Outlet_Size") := NULL]
+
 
 ## SEPERATING TRAIN AND TEST CLEAN DATA   View(head(cleanData))
 
@@ -133,9 +153,10 @@ clean.test <- cleanData[-(1:nrow(clean.train)),]
 
 # INITIATING H2O
 
- localH2o <- h2o.init(nthreads = -1)
-#localH2O <- h2o.init(ip='localhost', nthreads=-1,
-#                     min_mem_size='10G', max_mem_size='20G')
+# localH2o <- h2o.init(nthreads = -1)
+# rm(localH2)
+# localH2O <- h2o.init(ip='localhost', nthreads=-1,
+#                     min_mem_size='8G', max_mem_size='12G')
 # h2o.shutdown(prompt = T)
 
 # CONVERTING DATA SET TO H2O FORMAT | names(train.h2o) names(train) head(train.h2o)
@@ -144,15 +165,15 @@ test.h2o <- as.h2o(clean.test)
 
 ## SETTING COLUMN POSITIONS TO DEP AND INDEP VARIABLES
 
-xIndep <- c(1:13, 15, 18:20)
-yDep <- 14
+xIndep <- c(1:4, 8:11,13:19, 25,26)
+yDep <- 12
 
 ## RANDOM FOREST
-
+rm(rf.model)
 rf.model <- h2o.randomForest(y=yDep, x=xIndep, training_frame = train.h2o,
-                             ntrees = 1000, mtries = 5, max_depth = 13, seed = 1001)
+                             ntrees = 500, mtries = 10, max_depth = 20, seed = 1001)
 
-# h2o.varimp(rf.model)
+# View(h2o.varimp(rf.model))
 
 h2o.performance(rf.model)
 
@@ -200,7 +221,7 @@ df <- data.frame(Item_Identifier = test$Item_Identifier,
 ### GRADIENT BOOSTING
 
 gbm.model <- h2o.gbm(y=yDep, x=xIndep, training_frame = train.h2o, 
-                     ntrees = 1400, max_depth = 8, learn_rate = 0.1, seed = 100)
+                     ntrees = 1400, max_depth = 16, learn_rate = 0.1, seed = 100)
 
 
 h2o.performance(gbm.model)
@@ -211,12 +232,21 @@ gbm.pred <- as.data.frame(h2o.predict(gbm.model, test.h2o))
 
 #View(head(gbm.pred))
 
+df <- data.frame(Item_Identifier = clean.train$Item_Identifier,
+                 Outlet_Identifier = clean.train$Outlet_Identifier,
+                 Actual_Sales = clean.train$Item_Outlet_Sales,
+                 Predicted_Sales = gbm.pred$predict)
+
 df <- data.frame(Item_Identifier = test$Item_Identifier,
                  Outlet_Identifier = test$Outlet_Identifier,
                  Item_Outlet_Sales = gbm.pred$predict)
 
+train[, .(Avg_Sales = mean(Item_Outlet_Sales)), Outlet_Identifier][order(Outlet_Identifier)]
+data.table(df)[, .(Avg_Sales = mean(Item_Outlet_Sales)), Outlet_Identifier][order(Outlet_Identifier)]
+df$
 
-#write.csv(df, "./Output/BigMartSales_Siva_17_Submission.csv", row.names = F)
+
+#write.csv(df, "./Output/BigMartSales_Siva_18_Submission.csv", row.names = F)
 
 ######## DATA ANALYSIS ##########
 
