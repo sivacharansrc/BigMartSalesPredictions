6b26ecf6397aab9429d6c0b3ceba7cfd65f0d2b4
sivacharansrc <sivacharansrc@gmail.com>
2019-02-23 16:30:01 -0600

    All variable transformation completed

--- pyScript/pyScript.py
+++ pyScript/pyScript.py
@@ -176,3 +176,56 @@ upper_limit = np.percentile(df_clean['Item_Visibility_varTransformed'], 75) + (1
 df_clean['Item_Visibility_varTransformed'] = np.sqrt(df_clean['Item_Visibility_varTransformed'])
 
 # THE OUTLIERS ARE REDUCED FROM 144 TO 29. ALSO THE ACTUAL DISTRIBUTION WAS RIGHT SKEWED. THIS IS NOW FIXED BY TAKING THE SQRT TRANSFORMATION
+
+# FEATURE ENGINEERING FOR OUTLET ESTABLISHMENT YEAR
+
+df_clean['No_Of_Years_Established_varTransformed'] = 2013 - df_clean['Outlet_Establishment_Year']
+df_clean[['Outlet_Identifier', 'Outlet_Establishment_Year', 'No_Of_Years_Established_varTransformed']].head(30)
+df_clean['No_Of_Years_Established_varTransformed'].isnull().value_counts()
+
+# THE NO OF YEARS ESTABLISHED CAN BE USED IN MODEL INSTEAD OF ESTABLISHMENT YEAR. THERE ARE NO MISSING VALUES AND THIS FEATURE IS GOOD TO BE USED
+
+# FIX THE ITEM FAT CONTENT VARIABLE
+
+df_clean['Item_Fat_Content_varTransformed'] = df_clean['Item_Fat_Content']
+df_clean['Item_Fat_Content_varTransformed'].value_counts()
+df_clean.loc[df_clean['Item_Fat_Content_varTransformed'].isin(['LF', 'low fat']), 'Item_Fat_Content_varTransformed'] = 'Low Fat'
+df_clean.loc[df_clean['Item_Fat_Content_varTransformed'] == 'reg', 'Item_Fat_Content_varTransformed'] = 'Regular'
+df_clean['Item_Fat_Content_varTransformed'].value_counts()
+
+# THE TYPOS IN THE ITEM FAT CONTENT VARIABLE IS SUCCESSFULLY FIXED
+
+# CHECK FEATURE ENGINEERING FOR FEATURE TYPE - PENDING
+
+# OUTLET SIZE DATA IS MISSING. CHECK FROM OUTLET IDENTIFIER OR USING ITEM OUTLET SALES AS A PROXY
+df_clean['Outlet_Size_varTransformed'] = df_clean['Outlet_Size']
+# df_clean['Outlet_Identifier'][df_clean['Outlet_Size'].isnull()].head(20)
+# pd.crosstab(df_clean['Outlet_Type'], df_clean['Outlet_Size_varTransformed'])
+# From the cross tab, we can assume that all Grocery Stores are small, Supermarket2 and 3 are mostly medium, while Supermarket 1 can be of any size
+df_clean.loc[(df_clean['Outlet_Size_varTransformed'].isnull()) & (df_clean['Outlet_Type'] == 'Grocery Store'), 'Outlet_Size_varTransformed'] = 'Small'
+df_clean.loc[(df_clean['Outlet_Size_varTransformed'].isnull()) & (df_clean['Outlet_Type'].isin(['Supermarket Type3', 'Supermarket Type2'])),
+             'Outlet_Size_varTransformed'] = 'Medium'
+
+# WE YET NEED TO FIX THE VALUES FOR SUPERMARKET TYPE1
+# df_clean.groupby(['Outlet_Size_varTransformed', 'Outlet_Type', 'Outlet_Location_Type']).agg({'Item_Outlet_Sales': 'sum'}).reset_index()
+# NO CONCLUSION CAN BE REACHED WITH THE OUTLET SALES
+
+pd.crosstab(df_clean['Outlet_Size_varTransformed'][df_clean['Outlet_Type'] == 'Supermarket Type1'], df_clean['Outlet_Location_Type'][df_clean['Outlet_Type'] == 'Supermarket Type1'])
+# FROM THE NUMBERS LET US ASSUME THAT TIER 3 IS HIGH, TIER 2 IS MEDIUM AND TIER 1 IS SMALL
+# df_clean['Outlet_Size_varTransformed'][df_clean['Outlet_Type'] == 'Supermarket Type2'].mode()[0]
+# df_clean[['Outlet_Identifier', 'Outlet_Location_Type']][df_clean['Outlet_Size_varTransformed'].isnull()].drop_duplicates()
+
+df_clean.loc[(df_clean['Outlet_Size_varTransformed'].isnull()) & (df_clean['Outlet_Location_Type'] == 'Tier 1'), 'Outlet_Size_varTransformed'] = 'Small'
+df_clean.loc[(df_clean['Outlet_Size_varTransformed'].isnull()) & (df_clean['Outlet_Location_Type'] == 'Tier 2'), 'Outlet_Size_varTransformed'] = 'Medium'
+df_clean.loc[(df_clean['Outlet_Size_varTransformed'].isnull()) & (df_clean['Outlet_Location_Type'] == 'Tier 3'), 'Outlet_Size_varTransformed'] = 'High'
+
+# ALL VARIABLES ARE FIXED FOR OUTLIERS AND MISSING DATA
+
+df_final = df_clean[['Item_Identifier', 'Item_MRP', 'Item_Outlet_Sales', 'Item_Visibility_varTransformed', 'Item_Weight_varTransformed',
+                     'Outlet_Identifier', 'Outlet_Location_Type', 'Outlet_Type', 'No_Of_Years_Established_varTransformed',
+                     'Item_Fat_Content_varTransformed', 'Outlet_Size_varTransformed']]
+df_final = df_final.reset_index()
+# SAMPLING DATA
+
+x_train = df_final.sample(frac=0.8, random_state=1)
+y_test = df_final.loc[~df_final.index.isin(x_train.index)]
